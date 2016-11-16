7fe5a94a67e5e4266e4919517bc79564f82a3133
gerson22 <gerson.isaias.rivera@gmail.com>
2016-11-16 12:19:28 -0600

    Subi todas las carpetas necesarias ahora si

--- app/src/main/java/placesafe/placesafe/mainActivity.java
+++ app/src/main/java/placesafe/placesafe/mainActivity.java
@@ -1,13 +1,72 @@
 package placesafe.placesafe;
 
+import android.app.Activity;
+import android.content.Intent;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class mainActivity extends AppCompatActivity {
+
+public class mainActivity extends Activity {
+
+    int PLACE_PICKER_REQUEST = 1;
+    TextView getPlaces;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPlaces = (TextView) findViewById(R.id.placePick);
+        final Activity act = this;
+        getPlaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                Intent intent;
+                try {
+                    intent = builder.build(act);
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this,data);
+                String address = String.format("Place: %s", place.getAddress());
+                getPlaces.setText(address);
+            }
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+
+
+    }
+
+
 }
