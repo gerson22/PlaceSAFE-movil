0341868cf90e00a489d23dd4c68e6f7c9b531c15
gerson22 <gerson.isaias.rivera@gmail.com>
2016-11-25 17:47:14 -0600

    Cambios restantes para class RequestVolley

--- app/src/main/java/placesafe/placesafe/RequestVolley.java
+++ app/src/main/java/placesafe/placesafe/RequestVolley.java
@@ -1,7 +1,88 @@
 package placesafe.placesafe;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Gerson on 25/11/16.
  */
 public class RequestVolley {
+    private static RequestVolley _rv = null;
+    private Context context;
+    private final String URI = "https://www.curiosity.com.mx";
+
+    private HashMap<String, Integer> methods = new HashMap<String, Integer>();
+    private RequestVolley(Context context){
+        this.context = context;
+        initMethods();
+    }
+
+    public static RequestVolley getInstance(Context context){
+        _rv = (_rv == null) ? new RequestVolley(context) : _rv;
+        return _rv;
+    }
+
+    private void initMethods(){
+        methods.put("GET",Request.Method.GET);
+        methods.put("POST",Request.Method.POST);
+        methods.put("HEAD",Request.Method.HEAD);
+        methods.put("DELETE",Request.Method.DELETE);
+        methods.put("PUT",Request.Method.PUT);
+
+    }
+
+    public void requestString(String method,String path, Response.Listener<String> listener){
+        Toast.makeText(context,method + " - "+ path,Toast.LENGTH_LONG).show();
+        StringRequest stringRequest = new StringRequest(methods.get(method),URI + path ,
+                listener,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return null;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+    }
+
+    public void requestString(String method,String path, Response.Listener listener, final HashMap<String,String> params){
+        StringRequest stringRequest = new StringRequest(methods.get(method),URI + path ,
+                listener,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+    }
+
+
+
 }
--- app/src/main/java/placesafe/placesafe/mainActivity.java
+++ app/src/main/java/placesafe/placesafe/mainActivity.java
@@ -1,13 +1,120 @@
 package placesafe.placesafe;
 
+import android.app.Activity;
+import android.content.Intent;
+
+import com.android.volley.Response;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class mainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class mainActivity extends Activity {
+
+    int PLACE_PICKER_REQUEST = 1;
+    TextView getPlaces;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPlaces = (TextView) findViewById(R.id.placePick);
+        final Activity act = this;
+        try {
+            pruebaGETVolley();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        getPlaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                Intent intent;
+                try {
+                    intent = builder.build(act);
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this,data);
+                String address = String.format("Place: %s", place.getAddress());
+                Toast.makeText(this, address, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    public void pruebaGETVolley() throws JSONException {
+        //RequestVolley is a class to make request to the server
+        RequestVolley rv = RequestVolley.getInstance(getApplicationContext());
+
+        //Response.Listener is a listener to get success response
+        Response.Listener<String> listener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                JSONObject obj;
+                try {
+                    obj = new JSONObject(response);
+                    int estado = Integer.parseInt(obj.optString("status"));
+                    String message = obj.optString("message");
+                    Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        /*
+        *   El metodo requestString recibe
+        *   1.- Methodo
+        *   2.- Path o ruta
+        *   3.- Escuchador
+        *   4.- Datos a enviar (Opcional)
+        * */
+        rv.requestString("GET", "/android",listener );
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+
+
+    }
+
+
 }
