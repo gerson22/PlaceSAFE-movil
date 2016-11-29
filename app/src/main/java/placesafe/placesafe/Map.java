b6f5e0b9777aaf6576d9ed97002797431895a0ad
gerson22 <gerson.isaias.rivera@gmail.com>
2016-11-28 18:10:04 -0600

    Cambios Willy :D

--- app/src/main/java/placesafe/placesafe/Map.java
+++ app/src/main/java/placesafe/placesafe/Map.java
@@ -22,6 +22,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Map extends Activity implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
 
+    GoogleMap mapp;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,25 +35,35 @@ public class Map extends Activity implements GoogleMap.OnMarkerClickListener, On
 
     @Override
     public void onMapReady(GoogleMap map) {
+        mapp = map;
         LatLng torreon = new LatLng(25.5400882, -103.4236984);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(torreon, 13));
         map.getUiSettings().setZoomControlsEnabled(true);
         map.getUiSettings().setCompassEnabled(true);
         map.getUiSettings().setZoomGesturesEnabled(true);
         map.getUiSettings().setTiltGesturesEnabled(true);
         map.getUiSettings().setRotateGesturesEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            map.setMyLocationEnabled(true);
+        }
+        else{
+//            Toast.makeText(this, "No supported", Toast.LENGTH_LONG).show();
+        }
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(torreon, 13));
         makePoint(map, torreon, "Torreón", "Ya me fastidio");
     }
 
-    public void makePoint(GoogleMap map, LatLng pos, String tit, String snip){
+    public void makePoint(GoogleMap map, LatLng pos, String tit, String snip) {
         map.addMarker(new MarkerOptions()
-            .position(pos)
-            .title(tit)
-            .snippet(snip));
+                .position(pos)
+                .title(tit)
+                .snippet(snip));
         map.setOnMarkerClickListener(this);
     }
 
