2032e7af27ec701f02388cf18997d623e686201a
willy95 <wilvardo@gmail.com>
2016-11-29 06:52:20 -0600

    Agregando icono y comenzando guardar comentario

--- app/src/main/java/placesafe/placesafe/place.java
+++ app/src/main/java/placesafe/placesafe/place.java
@@ -15,6 +15,9 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
 /**
@@ -23,6 +26,7 @@ import java.util.HashMap;
 public class place extends Activity {
 
     ImageButton btnBack;
+    Button btnComment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +34,19 @@ public class place extends Activity {
         setContentView(R.layout.view_lugar);
 
         btnBack = (ImageButton) findViewById(R.id.back);
+        btnComment = (Button) findViewById(R.id.send_comment);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 back();
+            }
+        });
+
+        btnComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comment();
             }
         });
 
@@ -44,5 +56,24 @@ public class place extends Activity {
     protected void back(){
         final Intent intentMap = new Intent(getApplicationContext(), Map.class);
         startActivity(intentMap);
+    }
+
+    protected void comment(){
+        RequestVolley req = RequestVolley.getInstance(getApplicationContext());
+        HashMap<String, String> data = new HashMap<>();
+        data.put("Comment", "Nuevo Comentario");
+        req.requestString("POST", "/commentNew", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(place.this, "Hola", Toast.LENGTH_LONG).show();
+//                JSONObject obj;
+//                try {
+//                    obj = new JSONObject(response);
+//                    Toast.makeText(place.this, obj.toString(), Toast.LENGTH_LONG).show();
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        }, data);
     }
 }
