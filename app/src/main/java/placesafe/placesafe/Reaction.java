90985e63530af483560907183a9649354d90dc5c
gerson22 <gerson.isaias.rivera@gmail.com>
2016-12-01 14:08:00 -0600

    Modulo de reaccion lista ya guarda :)

--- app/src/main/java/placesafe/placesafe/Reaction.java
+++ app/src/main/java/placesafe/placesafe/Reaction.java
@@ -1,15 +1,19 @@
 package placesafe.placesafe;
 
+import android.os.Bundle;
+
 /**
  * Created by Gerson on 01/12/16.
  */
 public class Reaction {
     private String url_image;
     private String name;
+    private Bundle bundle;
 
-    public Reaction(String name, String url_image) {
+    public Reaction(String name, String url_image,Bundle bundle) {
         this.name = name;
         this.url_image = url_image;
+        this.bundle = bundle;
     }
 
     public String getName() {
@@ -26,5 +30,12 @@ public class Reaction {
 
     public void setUrl_image(String url_image) {
         this.url_image = url_image;
+    }
+    public Bundle getBundle() {
+        return bundle;
+    }
+
+    public void setBundle(Bundle bundle) {
+        this.bundle = bundle;
     }
 }
